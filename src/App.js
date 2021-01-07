dee01bf7890090b802e76e06c46346b5dd1fe090
Anushka Gupta <42449841+anushka23g@users.noreply.github.com>
2021-01-08 05:26:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+
+  const [value,setv]=useState("");
+  const [items,setitem]=useState([]);
+  
+  function add(event){
+setv(event.target.value);
+  }
+
+  function addtolist(){
+setitem((prevValue) =>{
+return [...prevValue,value];
+})
+setv("");
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+      <h1>Todo List</h1>
+      <input type="text" name="items" value={value} onChange={add}/>
+      <button onClick={addtolist}>Add to list</button>
+       <div>
+         <ul>
+          { items.map((item)=>{
+
+           return (<li>{item} </li>);
+          
+          })}
+           </ul>
+       </div>
+       </div>
   );
 }
